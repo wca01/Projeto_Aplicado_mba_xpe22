d152ce49a60c9c93eb0a9c4b3ecc725a11d92c30
wca01 <welbboy@live.com>
2022-10-14 11:04:29 -0300

    add spark jars in .py

--- dags/pyspark/olist_converte_parquet.py
+++ dags/pyspark/olist_converte_parquet.py
@@ -9,7 +9,7 @@ SparkConf()
     .set("spark.hadoop.fs.s3a.fast.upload", True)
     .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     .set('spark.hadoop.fs.s3a.aws.credentials.provider', 'com.amazonaws.auth.EnvironmentVariableCredentialsProvider')
-    .set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3')
+    .set('spark.jars.packages', 'com.amazonaws:aws-java-sdk:1.7.4', 'org.apache.hadoop:hadoop-aws:2.7.3')
 )
 
 # apply config
