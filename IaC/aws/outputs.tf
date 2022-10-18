718a5e32d05517ebd183950b9c8b02210c09e5a3
wca01 <welbboy@live.com>
2022-10-18 16:54:24 -0300

    clean files

--- IaC/aws/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "region" {
-  description = "AWS region"
-  value       = var.region
-}
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = local.cluster_name
-}
--- IaC/aws/provider.tf
+++
@@ -1,8 +0,0 @@
-# criand backend do terraform
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-mba2022"
-    key    = "projeto-mba22/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
