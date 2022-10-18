90b0e1063a46b9c2b9f6c4ab50f9858b25dc1332
wca01 <welbboy@live.com>
2022-10-18 18:56:57 -0300

    edit ignored files

--- dags/pyspark/olist_entregadores_vendores.py
+++ dags/pyspark/olist_entregadores_vendores.py
@@ -9,7 +9,7 @@ SparkConf()
     .set("spark.hadoop.fs.s3a.fast.upload", True)
     .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     .set('spark.hadoop.fs.s3a.aws.credentials.provider', 'com.amazonaws.auth.EnvironmentVariableCredentialsProvider')
-    .set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3')
+    .set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2')
 )
 
 # apply config
